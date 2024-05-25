1238cb7e1d0d365a62498a424bd47802e44bfae9
Lukas Drews <80822758+LukDrews@users.noreply.github.com>
2024-05-25 18:32:48 +0200

    Update Copy Button

--- app.config.ts
+++ app.config.ts
@@ -38,5 +38,12 @@ export default defineAppConfig({
     form: {
       successMessage: 'Message sent. Thank you!'
     }
+  },
+
+  prose: {
+    copyButton: {
+      iconCopy: 'i-material-symbols-eda-rounded',
+      iconCopied: 'i-material-symbols-light-helicopter-sharp'
+    }
   }
 })
